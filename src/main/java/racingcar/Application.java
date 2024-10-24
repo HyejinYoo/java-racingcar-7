52f9dd9d3f5f6fecd680504d6ba275f190f27c6c
HyejinYoo <yyyhj0114@gmail.com>
2024-10-24 23:02:21 +0900

    fix: 자동차 이름 글자수 제한 예외처리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,14 +4,18 @@ import java.util.Arrays;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String inputNames = Console.readLine();
-        String[] carNames = inputNames.split(",");
+    	try {
+            System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+            String inputNames = Console.readLine();
+            String[] carNames = inputNames.split(",");
 
-        System.out.println("시도할 횟수는 몇 회인가요?");
-        int rounds = Integer.parseInt(Console.readLine());
+            System.out.println("시도할 횟수는 몇 회인가요?");
+            int rounds = Integer.parseInt(Console.readLine());
 
-        Race race = new Race(Arrays.asList(carNames));
-        race.play(rounds);
+            Race race = new Race(Arrays.asList(carNames));
+            race.play(rounds);
+        } catch (IllegalArgumentException e) {
+            throw e;
+        }
     }
 }
